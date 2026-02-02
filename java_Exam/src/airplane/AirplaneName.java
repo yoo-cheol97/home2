dc464205e1acd49e50a6c7658afc0e9d26649062
yoocheol-shin <144026708+yoocheol-shin@users.noreply.github.com>
2026-02-03 01:29:15 +0900

    homework~0203

---
+++ java_Exam/src/airplane/AirplaneName.java
@@ -0,0 +1,33 @@
+package airplane;
+
+public class AirplaneName {
+	
+	private int number;
+	private String seatOx;
+	
+	public AirplaneName(int number, String seatOx) {
+		this.number = number;
+		this.seatOx = seatOx;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+
+	public String getSeatOx() {
+		return seatOx;
+	}
+
+	public void setSeatOx(String seatOx) {
+		this.seatOx = seatOx;
+	}
+
+	@Override
+	public String toString() {
+		return this.number + " : " + this.seatOx;
+	}
+	
+}
+	
+
+
