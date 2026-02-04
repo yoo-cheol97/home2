7797d2c24455490cb1749f1dedf987fab4e91cc9
yoocheol-shin <144026708+yoocheol-shin@users.noreply.github.com>
2026-02-04 22:21:35 +0900

    homework~0204

---
+++ java_Exam/src/mart/StoreTest.java
@@ -0,0 +1,19 @@
+package mart;
+
+public class StoreTest {
+	
+	public static void main(String[] args) {
+		Store store= new Store();
+		
+		store.add(new Item("A", "2026-02-04"));
+		store.add(new Item("B", "2026-02-03"));
+		store.add(new Item("C", "2026-02-02"));
+		store.add(new Item("D", "2026-02-01"));
+		store.add(new Item("E", "2026-02-05"));
+		store.add(new Item("F", "2026-02-06"));
+		
+		System.out.println(store);
+		
+	}
+	
+}
